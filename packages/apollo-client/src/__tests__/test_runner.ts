1724ac31385f862eab6f7cebaefe5f39c1a9bbd3
Ricardo Portugal <ricardofmportugal@gmail.com>
2017-12-07 20:42:37 +0000

    run tests for ReduxCache

--- packages/apollo-client/src/__tests__/test_runner.ts
+++ packages/apollo-client/src/__tests__/test_runner.ts
@@ -1,4 +1,6 @@
 import runTests from '../tests_gen';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+// import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ReduxCache } from 'apollo-cache-redux';
 
-runTests(InMemoryCache);
+// runTests(InMemoryCache);
+runTests(ReduxCache);
